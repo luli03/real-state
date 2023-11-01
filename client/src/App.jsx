eff208e36a75d1680a28163bb595b16864c12646
Joed Joven <wog.jjoven@gmail.com>
2023-11-01 17:00:20 +0800

    initial pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/SignUp";
 
 function App() {
+
   return (
-    <h1 className="text-3xl-red font-bold">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
